ed924d9782b6afa83fabb5f0ae4ccc48af0c46ff
Simon McLain <G00364775@GMIT.iegit config --global user.name SimonMcLaingit config --global user.email G00364775@GMIT.ie>
2018-04-15 21:07:27 +0100

    test files added

---
+++ openfile.py
@@ -0,0 +1,39 @@
+# Simon McLain 2018-03-04
+# Open Iris dataset script
+
+import csv
+
+with open("data/iris.csv") as f:
+  for line in f:
+    a = line.split(',')
+    print (a[0], a[1], a[2], a[3])
+    
+
+    
+
+    
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+    
+  
+
+
+    
+
+
+ 
+ 
+  #for line in f:
+    # print(line.split(',')[0].format(*line)
+  
